258951409fe7093bd688ae09e655462e80e0a9aa
silvak <silvak.jeg@gmail.com>
2023-04-30 02:28:54 -0400

    add new page and clean the proyect

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
+app.options("*", cors());
 
-app.use("/", express.static(__dirname + "/public"));
+app.use("/", express.static(__dirname + "/public/index.html"));
 app.get("/", async (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+});
+
+app.use("/news", express.static(__dirname + "/public/news.html"));
+app.get("/news", async (req, res) => {
+  res.sendFile(__dirname + "/public/news.html");
 });
 
 const apiProxy1 = createProxyMiddleware("/", {
@@ -16,6 +26,17 @@ const apiProxy1 = createProxyMiddleware("/", {
 const apiProxy2 = createProxyMiddleware("/business", {
   target: "https://www.reuters.com",
   changeOrigin: true,
+});
+
+app.get("/noadd", async (req, res) => {
+  try {
+    const response = await axios.get(`http://localhost:3000/business`);
+    const $ = cheerio.load(response.data);
+    $(".ad-slot__container__FEnoz").remove(); // Eliminar todos los elementos con la clase 'clase-a-eliminar' del DOM
+    res.send($.html());
+  } catch (error) {
+    res.send(`Error: ${error.message}`);
+  }
 });
 
 app.use(apiProxy1);
